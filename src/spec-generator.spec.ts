523c11dc25e2c761ad4ea24a1071513491e3b3f3
Rinto Jose <rinto.jose@tcs.com>
2017-02-03 21:17:30 +0530

    add quote and semicolon configurations

--- src/spec-generator.spec.ts
+++ src/spec-generator.spec.ts
@@ -167,3 +167,277 @@ describe('generateSpec', () => {
   })
 
 })
+
+describe('generateSpec with options quote = double quote', () => {
+
+  it('should generate spec with double quote if given empty object', () => {
+    let result = generateSpec({}, { quote: '"' })
+    expect(result).be.a('array');
+    expect(result).be.length(1);
+    expect(result[0]).be.equal('expect(result).be.a("object");');
+  })
+
+  it('should generate spec with double quote if given empty array', () => {
+    let result = generateSpec([], { quote: '"' })
+    expect(result).be.a('array');
+    expect(result).be.length(3);
+    expect(result[0]).be.equal('expect(result).be.a("array");');
+    expect(result[1]).be.equal('expect(result).be.length(0);');
+  })
+
+  it('should generate spec with double quote if given array of strings', () => {
+    let result = generateSpec(['a', 'b', 'c'], { quote: '"' })
+    expect(result).be.a('array');
+    expect(result).be.length(6);
+    expect(result[0]).be.equal('expect(result).be.a("array");');
+    expect(result[1]).be.equal('expect(result).be.length(3);');
+    expect(result[2]).be.equal('expect(result[0]).be.equal("a");');
+    expect(result[3]).be.equal('expect(result[1]).be.equal("b");');
+    expect(result[4]).be.equal('expect(result[2]).be.equal("c");');
+  })
+
+  it('should generate spec with double quote if given an object', () => {
+    let result = generateSpec({ firstName: 'John', lastName: 'Doe' }, { quote: '"' })
+    expect(result).be.a('array');
+    expect(result).be.length(7);
+    expect(result[0]).be.equal('expect(result).be.a("object");');
+    expect(result[1]).be.equal('expect(result).have.property("firstName");');
+    expect(result[2]).be.equal('expect(result.firstName).be.equal("John");');
+    expect(result[3]).be.equal('');
+    expect(result[4]).be.equal('expect(result).have.property("lastName");');
+    expect(result[5]).be.equal('expect(result.lastName).be.equal("Doe");');
+    expect(result[6]).be.equal('');
+  })
+
+  it('should generate spec with double quote if given an array of objects', () => {
+    let result = generateSpec([
+      { firstName: 'John', lastName: 'Doe' },
+      { firstName: 'John', lastName: 'Doe' }
+    ], { quote: '"' })
+
+    expect(result).be.a('array');
+    expect(result).be.length(16);
+    expect(result[0]).be.equal('expect(result).be.a("array");');
+    expect(result[1]).be.equal('expect(result).be.length(2);');
+    expect(result[2]).be.equal('expect(result[0]).be.a("object");');
+    expect(result[3]).be.equal('expect(result[0]).have.property("firstName");');
+    expect(result[4]).be.equal('expect(result[0].firstName).be.equal("John");');
+    expect(result[5]).be.equal('');
+    expect(result[6]).be.equal('expect(result[0]).have.property("lastName");');
+    expect(result[7]).be.equal('expect(result[0].lastName).be.equal("Doe");');
+    expect(result[8]).be.equal('');
+    expect(result[9]).be.equal('expect(result[1]).be.a("object");');
+    expect(result[10]).be.equal('expect(result[1]).have.property("firstName");');
+    expect(result[11]).be.equal('expect(result[1].firstName).be.equal("John");');
+    expect(result[12]).be.equal('');
+    expect(result[13]).be.equal('expect(result[1]).have.property("lastName");');
+    expect(result[14]).be.equal('expect(result[1].lastName).be.equal("Doe");');
+    expect(result[15]).be.equal('');
+  })
+
+  it('should generate spec with double quote if object with inner objects is given', () => {
+    let result = generateSpec({
+      firstName: 'John',
+      lastName: 'Doe',
+      manager: { firstName: 'John', lastName: 'Doe' }
+    }, { quote: '"' })
+    expect(result).be.a('array');
+    expect(result).be.length(15);
+    expect(result[0]).be.equal('expect(result).be.a("object");');
+    expect(result[1]).be.equal('expect(result).have.property("firstName");');
+    expect(result[2]).be.equal('expect(result.firstName).be.equal("John");');
+    expect(result[3]).be.equal('');
+    expect(result[4]).be.equal('expect(result).have.property("lastName");');
+    expect(result[5]).be.equal('expect(result.lastName).be.equal("Doe");');
+    expect(result[6]).be.equal('');
+    expect(result[7]).be.equal('expect(result).have.property("manager");');
+    expect(result[8]).be.equal('expect(result.manager).be.a("object");');
+    expect(result[9]).be.equal('expect(result.manager).have.property("firstName");');
+    expect(result[10]).be.equal('expect(result.manager.firstName).be.equal("John");');
+    expect(result[11]).be.equal('');
+    expect(result[12]).be.equal('expect(result.manager).have.property("lastName");');
+    expect(result[13]).be.equal('expect(result.manager.lastName).be.equal("Doe");');
+    expect(result[14]).be.equal('');
+  })
+
+  it('should quote property name with double quote if it contains special character', () => {
+    let result = generateSpec({ 'Content-Type': 'application/json' }, { quote: '"' })
+    expect(result).be.a('array');
+    expect(result).be.length(4);
+    expect(result[0]).be.equal('expect(result).be.a("object");');
+    expect(result[1]).be.equal('expect(result).have.property("Content-Type");');
+    expect(result[2]).be.equal('expect(result["Content-Type"]).be.equal("application/json");');
+    expect(result[3]).be.equal('');
+  })
+
+  it('should generate spec with double quote if an object with one of the property array is given', () => {
+    let result = generateSpec({ names: ['John', 'Doe'] }, { quote: '"' })
+    expect(result).be.a('array');
+    expect(result).be.length(7);
+    expect(result[0]).be.equal('expect(result).be.a("object");');
+    expect(result[1]).be.equal('expect(result).have.property("names");');
+    expect(result[2]).be.equal('expect(result.names).be.a("array");');
+    expect(result[3]).be.equal('expect(result.names).be.length(2);');
+    expect(result[4]).be.equal('expect(result.names[0]).be.equal("John");');
+    expect(result[5]).be.equal('expect(result.names[1]).be.equal("Doe");');
+    expect(result[6]).be.equal('');
+  })
+
+  it('should not quote the property value if it is not string', () => {
+    let result = generateSpec({ age: 20, active: false }, { quote: '"' })
+    expect(result).be.a('array');
+    expect(result).be.length(7);
+    expect(result[0]).be.equal('expect(result).be.a("object");');
+    expect(result[1]).be.equal('expect(result).have.property("age");');
+    expect(result[2]).be.equal('expect(result.age).be.equal(20);');
+    expect(result[3]).be.equal('');
+    expect(result[4]).be.equal('expect(result).have.property("active");');
+    expect(result[5]).be.equal('expect(result.active).be.equal(false);');
+    expect(result[6]).be.equal('');
+  })
+})
+
+describe('generateSpec with option semicolon false', () => {
+
+  it('should generate spec without semicolon if given true', () => {
+    let result = generateSpec(true, { semicolon: false })
+    expect(result).be.a('array');
+    expect(result).be.length(1);
+    expect(result[0]).be.equal('expect(result).be.equal(true)');
+  })
+
+  it('should generate spec without semicolon if given false', () => {
+    let result = generateSpec(false, { semicolon: false })
+    expect(result).be.a('array');
+    expect(result).be.length(1);
+    expect(result[0]).be.equal('expect(result).be.equal(false)');
+  })
+
+  it('should generate spec without semicolon if given defined', () => {
+    let result = generateSpec('defined', { special: true, semicolon: false })
+    expect(result).be.a('array');
+    expect(result).be.length(1);
+    expect(result[0]).be.equal('expect(result).be.defined');
+  })
+
+  it('should generate spec without semicolon if given undefined', () => {
+    let result = generateSpec(undefined, { semicolon: false })
+    expect(result).be.a('array');
+    expect(result).be.length(1);
+    expect(result[0]).be.equal('expect(result).be.undefined');
+  })
+
+  it('should generate spec without semicolon if given empty object', () => {
+    let result = generateSpec({}, { semicolon: false })
+    expect(result).be.a('array');
+    expect(result).be.length(1);
+    expect(result[0]).be.equal('expect(result).be.a(\'object\')');
+  })
+
+  it('should generate spec without semicolon if given empty array', () => {
+    let result = generateSpec([], { semicolon: false })
+    expect(result).be.a('array');
+    expect(result).be.length(3);
+    expect(result[0]).be.equal('expect(result).be.a(\'array\')');
+    expect(result[1]).be.equal('expect(result).be.length(0)');
+  })
+
+  it('should generate spec without semicolon if given array of strings', () => {
+    let result = generateSpec(['a', 'b', 'c'], { semicolon: false })
+    expect(result).be.a('array');
+    expect(result).be.length(6);
+    expect(result[0]).be.equal('expect(result).be.a(\'array\')');
+    expect(result[1]).be.equal('expect(result).be.length(3)');
+    expect(result[2]).be.equal('expect(result[0]).be.equal(\'a\')');
+    expect(result[3]).be.equal('expect(result[1]).be.equal(\'b\')');
+    expect(result[4]).be.equal('expect(result[2]).be.equal(\'c\')');
+  })
+
+  it('should generate spec without semicolon if given an object', () => {
+    let result = generateSpec({ firstName: 'John', lastName: 'Doe' }, { semicolon: false })
+    expect(result).be.a('array');
+    expect(result).be.length(7);
+    expect(result[0]).be.equal('expect(result).be.a(\'object\')');
+    expect(result[1]).be.equal('expect(result).have.property(\'firstName\')');
+    expect(result[2]).be.equal('expect(result.firstName).be.equal(\'John\')');
+    expect(result[3]).be.equal('');
+    expect(result[4]).be.equal('expect(result).have.property(\'lastName\')');
+    expect(result[5]).be.equal('expect(result.lastName).be.equal(\'Doe\')');
+    expect(result[6]).be.equal('');
+  })
+
+  it('should generate spec without semicolon if given an array of objects', () => {
+    let result = generateSpec([
+      { firstName: 'John', lastName: 'Doe' },
+      { firstName: 'John', lastName: 'Doe' }
+    ], { semicolon: false })
+
+    expect(result).be.a('array');
+    expect(result).be.length(16);
+    expect(result[0]).be.equal('expect(result).be.a(\'array\')');
+    expect(result[1]).be.equal('expect(result).be.length(2)');
+    expect(result[2]).be.equal('expect(result[0]).be.a(\'object\')');
+    expect(result[3]).be.equal('expect(result[0]).have.property(\'firstName\')');
+    expect(result[4]).be.equal('expect(result[0].firstName).be.equal(\'John\')');
+    expect(result[5]).be.equal('');
+    expect(result[6]).be.equal('expect(result[0]).have.property(\'lastName\')');
+    expect(result[7]).be.equal('expect(result[0].lastName).be.equal(\'Doe\')');
+    expect(result[8]).be.equal('');
+    expect(result[9]).be.equal('expect(result[1]).be.a(\'object\')');
+    expect(result[10]).be.equal('expect(result[1]).have.property(\'firstName\')');
+    expect(result[11]).be.equal('expect(result[1].firstName).be.equal(\'John\')');
+    expect(result[12]).be.equal('');
+    expect(result[13]).be.equal('expect(result[1]).have.property(\'lastName\')');
+    expect(result[14]).be.equal('expect(result[1].lastName).be.equal(\'Doe\')');
+    expect(result[15]).be.equal('');
+  })
+
+  it('should generate spec without semicolon if object with inner objects is given', () => {
+    let result = generateSpec({
+      firstName: 'John',
+      lastName: 'Doe',
+      manager: { firstName: 'John', lastName: 'Doe' }
+    }, { semicolon: false })
+    expect(result).be.a('array');
+    expect(result).be.length(15);
+    expect(result[0]).be.equal('expect(result).be.a(\'object\')');
+    expect(result[1]).be.equal('expect(result).have.property(\'firstName\')');
+    expect(result[2]).be.equal('expect(result.firstName).be.equal(\'John\')');
+    expect(result[3]).be.equal('');
+    expect(result[4]).be.equal('expect(result).have.property(\'lastName\')');
+    expect(result[5]).be.equal('expect(result.lastName).be.equal(\'Doe\')');
+    expect(result[6]).be.equal('');
+    expect(result[7]).be.equal('expect(result).have.property(\'manager\')');
+    expect(result[8]).be.equal('expect(result.manager).be.a(\'object\')');
+    expect(result[9]).be.equal('expect(result.manager).have.property(\'firstName\')');
+    expect(result[10]).be.equal('expect(result.manager.firstName).be.equal(\'John\')');
+    expect(result[11]).be.equal('');
+    expect(result[12]).be.equal('expect(result.manager).have.property(\'lastName\')');
+    expect(result[13]).be.equal('expect(result.manager.lastName).be.equal(\'Doe\')');
+    expect(result[14]).be.equal('');
+  })
+
+  it('should quote property name without semicolon if it contains special character', () => {
+    let result = generateSpec({ 'Content-Type': 'application/json' }, { semicolon: false })
+    expect(result).be.a('array');
+    expect(result).be.length(4);
+    expect(result[0]).be.equal('expect(result).be.a(\'object\')');
+    expect(result[1]).be.equal('expect(result).have.property(\'Content-Type\')');
+    expect(result[2]).be.equal('expect(result[\'Content-Type\']).be.equal(\'application/json\')');
+    expect(result[3]).be.equal('');
+  })
+
+  it('should generate spec without semicolon if an object with one of the property array is given', () => {
+    let result = generateSpec({ names: ['John', 'Doe'] }, { semicolon: false })
+    expect(result).be.a('array');
+    expect(result).be.length(7);
+    expect(result[0]).be.equal('expect(result).be.a(\'object\')');
+    expect(result[1]).be.equal('expect(result).have.property(\'names\')');
+    expect(result[2]).be.equal('expect(result.names).be.a(\'array\')');
+    expect(result[3]).be.equal('expect(result.names).be.length(2)');
+    expect(result[4]).be.equal('expect(result.names[0]).be.equal(\'John\')');
+    expect(result[5]).be.equal('expect(result.names[1]).be.equal(\'Doe\')');
+    expect(result[6]).be.equal('');
+  })
+
+})
--- src/spec-generator.ts
+++ src/spec-generator.ts
@@ -9,7 +9,7 @@ interface Options {
   context?: string
   specs?: string[]
   special?: boolean
-  quote?: boolean
+  quote?: string
   quoteType?: string
   semicolon?: boolean
 }
@@ -18,8 +18,7 @@ export function generateSpec(target: any, options?: Options) {
   options = Object.assign({
     context: 'expect(result)',
     specs: [],
-    quote: true,
-    quoteType: 'single',
+    quote: '\'',
     semicolon: true,
     special: false
   }, options)
@@ -28,44 +27,52 @@ export function generateSpec(target: any, options?: Options) {
 }
 
 function generateSpecForChai(target: any, options: Options) {
+  const q = options.quote;
+  const s = options.semicolon ? ';' : ''
+
   if (options.special) {
     if (target === 'defined') {
-      return [`${options.context}.be.defined;`]
+      return [`${options.context}.be.defined${s}`]
     }
     return []
   }
+
   if (target instanceof Array) {
-    options.specs.push(options.context + '.be.a(\'array\');');
-    options.specs.push(options.context + '.be.length(' + target.length + ');');
+    options.specs.push(`${options.context}.be.a(${q}array${q})${s}`);
+    options.specs.push(`${options.context}.be.length(${target.length})${s}`);
     target.map(function (item, index) {
       generateSpecForChai(item, Object.assign({}, options, {
-        context: options.context.replace(/\)$/, '') + '[' + index + '])'
+        context: `${options.context.replace(/\)$/, '')}[${index}])`
       }));
     });
     newLine(options.specs);
+
   } else if (typeof target === 'object') {
-    options.specs.push(options.context + '.be.a(\'object\');');
+    options.specs.push(`${options.context}.be.a(${q}object${q})${s}`);
     Object.keys(target).map(function (key) {
-      options.specs.push(options.context + '.have.property(\'' + key + '\');');
-      let keyRef = /^[a-zA-Z0-9_]+$/g.test(key) ? `.${key}` : `['${key}']`;
+      options.specs.push(`${options.context}.have.property(${q}${key}${q})${s}`);
+      let keyRef = /^[a-zA-Z0-9_]+$/g.test(key) ? `.${key}` : `[${q}${key}${q}]`;
       let contextRef = `${options.context.replace(/\)$/, '')}${keyRef})`;
       if (target[key] instanceof Array) {
         generateSpecForChai(target[key], Object.assign({}, options, { context: contextRef }));
       } else if (typeof target[key] === 'object') {
         generateSpecForChai(target[key], Object.assign({}, options, { context: contextRef }));
       } else if (isNaN(target[key])) {
-        options.specs.push(contextRef + '.be.equal(\'' + target[key] + '\');');
+        options.specs.push(`${contextRef}.be.equal(${q}${target[key]}${q})${s}`);
       } else {
-        options.specs.push(contextRef + '.be.equal(' + target[key] + ');');
+        options.specs.push(`${contextRef}.be.equal(${target[key]})${s}`);
       }
       newLine(options.specs);
     });
+
   } else if (target == undefined) {
-    options.specs.push(options.context + '.be.undefined;');
+    options.specs.push(`${options.context}.be.undefined${s}`);
+
   } else if (typeof target === 'string') {
-    options.specs.push(options.context + '.be.equal(\'' + target + '\');');
+    options.specs.push(`${options.context}.be.equal(${q}${target}${q})${s}`);
+
   } else {
-    options.specs.push(options.context + '.be.equal(' + target + ');');
+    options.specs.push(`${options.context}.be.equal(${target})${s}`);
   }
 
   return options.specs;
